221d173c492b31981a297731d8250034843bc055
t440s12 <t440s12@t440s12-PC>
2017-03-11 16:04:42 +0800

    add by xialin

--- fh/src/com/fh/controller/system/ruiyun/buildingproject/BuildingProjectController.java
+++ fh/src/com/fh/controller/system/ruiyun/buildingproject/BuildingProjectController.java
@@ -3,19 +3,13 @@ package com.fh.controller.system.ruiyun.buildingproject;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.fh.controller.base.BaseController;
-import com.fh.entity.Page;
 import com.fh.service.system.BuildingProjectService;
-import com.fh.util.PageData;
-import com.fh.util.Tools;
 
 @Controller
 @RequestMapping( value = "buildingproject" )
@@ -24,6 +18,7 @@ public class BuildingProjectController extends BaseController{
 	@Autowired
 	private BuildingProjectService buildingProjectService;
 
+	/** servlet/jsp 页面访问 */
 	@RequestMapping ( value = "buildingprojectlist" )
 	@ResponseBody
 	public ModelAndView buildingProjectList(String buildingProjectName) throws Exception{
@@ -36,6 +31,7 @@ public class BuildingProjectController extends BaseController{
 		return mv;
 	}
 	
+	/** 匹配freemaker模板 */
 	@RequestMapping ( value = "buildingprojectlist2" )
 	@ResponseBody
 	public ModelAndView buildingProjectList2(String building_project_name) throws Exception{
